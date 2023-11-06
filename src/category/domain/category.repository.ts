c9ef00bfcb6232efaa87bee46a138abcdb9089c9
euller <eullertav@gmail.com>
2023-11-06 09:00:34 -0300

    criando interface de repositório para buscar personalizados

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,5 @@
-import { IRepository } from "../shared/domain/repository/category.interface";
-import { Uuid } from "../shared/domain/value-objects/uuid.vo";
+import { IRepository } from "../../shared/domain/repository/category.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
 export interface ICategoryRepository extends IRepository<Category, Uuid> {}
