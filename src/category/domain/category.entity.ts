62e363c17f5b878dba83ea0d161fc1ee4db35c21
euller <eullertav@gmail.com>
2023-11-07 09:09:48 -0300

    utilizando test data builder no teste do repositório em memória de categoria

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -2,6 +2,7 @@ import { Entity } from "../../shared/domain/entity";
 import { EntityValidationError } from "../../shared/domain/validators/validation-error";
 import { ValueObject } from "../../shared/domain/value-object";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryFakeBuilder } from "./category-faker.builder";
 import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryConstructorProps = {
@@ -48,6 +49,10 @@ export class Category extends Entity {
     const validator = CategoryValidatorFactory.create();
     const isValid = validator.validate(entity);
     if (!isValid) throw new EntityValidationError(validator.errors);
+  }
+
+  static fake() {
+    return CategoryFakeBuilder;
   }
 
   changeName(name: string): void {
