fc2a959ced059f22074c1ee2ef3c21a8c3472716
euller <eullertav@gmail.com>
2023-11-20 20:02:50 -0300

    criando repositório sequelize de categoria - parte 1

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,9 @@
-import { IRepository } from "../../shared/domain/repository/category.interface";
+import {
+  IRepository,
+  ISearchableRepository,
+} from "../../shared/domain/repository/category.interface";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
-export interface ICategoryRepository extends IRepository<Category, Uuid> {}
+export interface ICategoryRepository
+  extends ISearchableRepository<Category, Uuid> {}
